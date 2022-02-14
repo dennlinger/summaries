17d678f5ced390fcd53491e730d302345964bad9
Dennis Aumiller <dennis.aumiller@gmx.de>
2022-02-14 17:25:50 +0100

    Moving the tests to a different file, since they are now no longer testing DensityPlot's functionality. Also adding brief test case for proximity scorer.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,26 +2,26 @@ import unittest
 
 import spacy
 
-from summaries.analysis import DensityPlot
+from summaries.utils import find_closest_reference_matches, max_rouge_2_match
 
 
 class TestDensityPlot(unittest.TestCase):
     def setUp(self):
-        self.plot = DensityPlot()
+        self.processor = spacy.load("de_core_news_sm", disable=("ner",))
 
     def test_find_closest_reference_match(self):
 
-        extracted_doc = self.plot.processor("Das ist ein Test.")
-        reference_doc = self.plot.processor("Ein längerer Inhalt. Er besteht aus mehreren Tests. Das ist ein Test.")
+        extracted_doc = self.processor("Das ist ein Test.")
+        reference_doc = self.processor("Ein längerer Inhalt. Er besteht aus mehreren Tests. Das ist ein Test.")
 
-        self.assertEqual([2/3], self.plot.find_closest_reference_match(extracted_doc, reference_doc))
+        self.assertEqual([2/3], find_closest_reference_matches(extracted_doc, reference_doc))
 
     def test_max_rouge_2_match(self):
 
-        extracted_sentence = list(self.plot.processor("Er besteht aus mehreren merkwürdigen Tests").sents)[0]
-        reference_doc = self.plot.processor("Ein längerer Inhalt. Er besteht aus mehreren Tests. Das ist ein Test.")
+        extracted_sentence = list(self.processor("Er besteht aus mehreren merkwürdigen Tests").sents)[0]
+        reference_doc = self.processor("Ein längerer Inhalt. Er besteht aus mehreren Tests. Das ist ein Test.")
 
-        self.assertEqual(1/3, self.plot.max_rouge_2_match(extracted_sentence, reference_doc))
+        self.assertEqual(1/3, max_rouge_2_match(extracted_sentence, reference_doc))
 
 
 if __name__ == '__main__':
