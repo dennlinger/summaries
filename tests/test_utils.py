c6577f59762ecdad325c9f17b8065823a7254b3e
Dennis Aumiller <dennis.aumiller@gmx.de>
2022-02-15 13:16:57 +0100

    Adjutsing tests of utils to account for pre-computation of ngrmas.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,9 @@
 import unittest
 
 import spacy
+from rouge_score.rouge_scorer import _create_ngrams
 
-from summaries.utils import find_closest_reference_matches, max_rouge_2_match
+from summaries.utils import find_closest_reference_matches, max_rouge_2_match, RelevantSentence
 
 
 class TestDensityPlot(unittest.TestCase):
@@ -19,9 +20,14 @@ class TestDensityPlot(unittest.TestCase):
     def test_max_rouge_2_match(self):
 
         extracted_sentence = list(self.processor("Er besteht aus mehreren merkwürdigen Tests").sents)[0]
-        reference_doc = self.processor("Ein längerer Inhalt. Er besteht aus mehreren Tests. Das ist ein Test.")
 
-        self.assertEqual(1/3, max_rouge_2_match(extracted_sentence, reference_doc))
+        reference_doc = self.processor("Ein längerer Inhalt. Er besteht aus mehreren Tests. Das ist ein Test.")
+        reference_ngrams = [_create_ngrams([token.lemma_ for token in sentence], n=2)
+                            for sentence in reference_doc.sents]
+        reference_sentences = [sentence.text for sentence in reference_doc.sents]
+
+        expected_result = RelevantSentence("Er besteht aus mehreren Tests.", 0.6, 1/3)
+        self.assertEqual(expected_result, max_rouge_2_match(extracted_sentence, reference_sentences, reference_ngrams))
 
 
 if __name__ == '__main__':
