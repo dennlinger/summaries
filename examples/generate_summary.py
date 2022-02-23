e2270ebfc985a52c15dd5c8e7fb8bd897bfc273c
Dennis Aumiller <dennis.aumiller@gmx.de>
2022-02-23 18:13:03 +0100

    Adding faster example for DPR.

--- examples/generate_summary.py
+++ examples/generate_summary.py
@@ -10,11 +10,11 @@ from extract_relevant_sentence_list import load_text_file
 
 if __name__ == '__main__':
 
-    source_text = load_text_file("Aachen_Wiki.txt")
+    source_text = load_text_file("Aachen_Wiki_short.txt")
     oracle_keywords = ["Aachen", "Nordrhein-Westfalen", "Einwohner", "Aquae Granni", "Aachener Dom", "Universität",
                        "UNESCO", "Oche"]
     extractor = OracleExtractor(len(oracle_keywords), lang="de", given_keywords=oracle_keywords)
-    summ = AspectSummarizer(extractor, "frequency")
+    summ = AspectSummarizer(extractor, "dpr")
 
     print(summ.summarize(source_text))
 
