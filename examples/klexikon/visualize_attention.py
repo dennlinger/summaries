50711d3d7e248072b812dc7c6500f878132d9303
Dennis Aumiller <dennis.aumiller@gmx.de>
2022-02-25 12:08:06 +0100

    Enabling processing of all 10 scripts.

--- examples/klexikon/visualize_attention.py
+++ examples/klexikon/visualize_attention.py
@@ -57,10 +57,8 @@ if __name__ == '__main__':
             optimizer.step()
             optimizer.zero_grad()
 
-        break
-
-    result = model(input_ids=model_inputs["input_ids"], attention_mask=model_inputs["attention_mask"],
-                   decoder_input_ids=model_inputs["decoder_input_ids"], output_attentions=True,
-                   labels=model_inputs["decoder_input_ids"])
-    predicted_ids = torch.argmax(result.logits.detach().to("cpu"), dim=-1)
-    print(tokenizer.decode(predicted_ids[0]))
+        result = model(input_ids=model_inputs["input_ids"], attention_mask=model_inputs["attention_mask"],
+                       decoder_input_ids=model_inputs["decoder_input_ids"], output_attentions=True,
+                       labels=model_inputs["decoder_input_ids"])
+        predicted_ids = torch.argmax(result.logits.detach().to("cpu"), dim=-1)
+        print(tokenizer.decode(predicted_ids[0]))
