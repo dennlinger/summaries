9674d83698387e19b3105bf22ebaca0cafd5fa44
Dennis Aumiller <dennis.aumiller@gmx.de>
2022-03-17 17:00:20 +0100

    Adding density plot for wiki-lingua.

---
+++ experiments/dataset_density/wikilingua/compute_wikilingua_density_plot.py
@@ -0,0 +1,34 @@
+"""
+Similar to examples/density_plot.py, but computes it on the full WikiLingua (German) dataset.
+"""
+
+from datasets import load_dataset
+
+from tqdm import tqdm
+from summaries.analysis import DensityPlot
+from summaries.better_split import better_sentence_split
+
+if __name__ == '__main__':
+    dataset = load_dataset("wiki_lingua", "german", ignore_verifications=True)
+
+    dp = DensityPlot(max_num_bins=100)
+
+    for partition_name in ["train"]:
+        partition = dataset[partition_name]
+
+        reference_texts = []
+        summary_texts = []
+
+        # first_n = 5
+
+        for idx, sample in enumerate(tqdm(partition)):
+            reference_sentences = better_sentence_split(" ".join(sample["article"]["document"]))
+            summary_sentences = better_sentence_split(" ".join(sample["article"]["summary"]))
+            reference_texts.append(reference_sentences)
+            summary_texts.append(summary_sentences)
+
+            # if idx >= first_n:
+            #     break
+
+        dp.plot(reference_texts, summary_texts, out_fn=f"density_{partition_name}.png")
+
