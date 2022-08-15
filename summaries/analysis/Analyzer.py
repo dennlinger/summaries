270c899bd68131e9d7fc754eeb69f4b02e6629d8
Dennis Aumiller <dennis.aumiller@gmx.de>
2022-08-15 17:11:05 +0200

    Adding option to enable or disable lemmatization when talking about the tokens. Also adding several more functions to sanity-check samples, wrt empty strings, unexpected lengths (the summary is longer), or fully extractive samples.

--- summaries/analysis/Analyzer.py
+++ summaries/analysis/Analyzer.py
@@ -6,6 +6,7 @@ or simply checking for the amount of overlap with a reference text.
 """
 from typing import Optional, List, Union
 from collections import Counter
+import warnings
 
 from rouge_score.rouge_scorer import _create_ngrams, _score_lcs, _score_ngrams
 from spacy.language import Language
@@ -19,8 +20,9 @@ from ..utils import get_nlp_model, interpret_lang_code, find_closest_reference_m
 class Analyzer:
     processor: Optional[Language]
     lang_code: Optional[str]
+    lemmatize: bool
 
-    def __init__(self, processor: Optional[Language] = None, lang: Optional[str] = None):
+    def __init__(self, lemmatize: bool = False, processor: Optional[Language] = None, lang: Optional[str] = None):
         # Automatically load a model if none is passed.
         if processor is None:
             if lang is None:
@@ -32,13 +34,15 @@ class Analyzer:
         else:
             self.processor = processor
 
+        self.lemmatize = lemmatize
+
     def density_plot(self, references: List[List[str]], summaries: List[List[str]], out_fn: Optional[str] = None,
                      max_num_bins: int = 100) -> None:
         """
         Generates density plots based on normalized position of reference sentences. A density
         :param references: List of reference texts, split into sentences.
         :param summaries: List of summaries; can be either gold or system summaries. Split into sentences.
-        :param out_fn: File name where to store the plot.
+        :param out_fn: File name where to store the plot. Will only store the plot if `out_fn` is provided.
         :param max_num_bins: Maximum number of bins to use in the resulting plot.
         :return: No output, but resulting plot will be saved to disk if `out_fn` is specified.
         """
@@ -51,6 +55,9 @@ class Analyzer:
             if len(list(reference_doc)) > max_article_length:
                 max_article_length = len(list(reference_doc))
 
+            # FIXME: Currently does not respect the self.lemmatize option!
+            warnings.warn("`density_plot` currently does not respect the choice of `Analyzer.lemmatize`"
+                          "and will always lemmatize!")
             # Compute likely source-target alignments
             reference_positions.extend(find_closest_reference_matches(summary_doc, reference_doc, 2, self.processor))
 
@@ -84,7 +91,7 @@ class Analyzer:
         :return: Count of how many repetitions were counted for what n-gram.
         """
 
-        tokens = [token.text for token in self.processor(text)]
+        tokens = self._get_tokens(text)
         ngrams = _create_ngrams(tokens, n=n)
 
         encountered_ngrams = set()
@@ -104,8 +111,8 @@ class Analyzer:
         Will compute the fraction of tokens in the generated summary that are shared in a longest common subsequence
         with the reference text (verbatim copy).
         """
-        summary_tokens = [token.lemma_ for token in self.processor(summary)]
-        reference_tokens = [token.lemma_ for token in self.processor(reference)]
+        summary_tokens = self._get_tokens(summary)
+        reference_tokens = self._get_tokens(reference)
 
         # TODO: Verify that this is actually equivalent to the recall, and not precision.
         return _score_lcs(reference_tokens, summary_tokens).precision
@@ -114,8 +121,8 @@ class Analyzer:
         """
         Will compute the fraction of ngrams in the generated summary that are shared with the reference text.
         """
-        summary_tokens = [token.lemma_ for token in self.processor(summary)]
-        reference_tokens = [token.lemma_ for token in self.processor(reference)]
+        summary_tokens = self._get_tokens(summary)
+        reference_tokens = self._get_tokens(reference)
 
         summary_ngrams = _create_ngrams(summary_tokens, n=n)
         reference_ngrams = _create_ngrams(reference_tokens, n=n)
@@ -134,3 +141,67 @@ class Analyzer:
             return True
         else:
             return False
+
+    def is_summary_longer_than_reference(self, summary: str, reference: str, length_metric: str = "char") -> bool:
+        """
+        Checks whether a particular sample has a longer summary than reference, indicating a compression ratio <= 1.0.
+        This is a frequent sanity check for quality of samples. Also note that we flag samples that are the exact
+        same length. This can happen in rare instances, where the samples are the exact same text.
+        :param summary: Summary text.
+        :param reference: Reference text.
+        :param length_metric: Can be either of "char", "whitespace" or "token". The first two are faster approximations,
+            and should generally indicate the same result as the slower "token" method, which uses the processor to
+            tokenize the input text. This is mainly important for texts where there is a high difference expected
+            between "proper" tokenization and approximations.
+        :return: Will return a boolean indicating whether the summary is longer than the reference.
+        """
+
+        valid_methods = ["char", "whitespace", "token"]
+        if length_metric not in valid_methods:
+            raise ValueError(f"length_metric should be either of {valid_methods}.")
+
+        if length_metric == "char":
+            length_summary = len(summary)
+            length_reference = len(reference)
+        elif length_metric == "whitespace":
+            length_summary = len(summary.split(" "))
+            length_reference = len(reference.split(" "))
+        elif length_metric == "token":
+            length_summary = len(self._get_tokens(summary))
+            length_reference = len(self._get_tokens(reference))
+        else:
+            raise ValueError("Unexpected length metric passed!")
+
+        if length_summary >= length_reference:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def is_either_text_empty(summary: str, reference: str) -> bool:
+        """
+        Simple utility to check whether a sample has either an empty summary or reference text, both of which
+        invalidate a sample.
+        :param summary: Summary text.
+        :param reference: Reference text.
+        :return: Boolean indicating whether either summary or reference are empty
+        """
+        # TODO: Propose a clear list of stripped symbols, which also extend to unicode (e.g., \xa0), or HTML (e.g.,
+        #  &nbsp;) characters
+        if not summary.strip("\n\t ") or not reference.strip("\n\t "):
+            return True
+        else:
+            return False
+
+    def _get_tokens(self, text: str) -> List[str]:
+        """
+        Helper method that reduces overhead of changing code parts with, for example, lemmatization choices.
+        :param text: Input text that should be tokenized.
+        :return: List of tokens in the original text.
+        """
+
+        if self.lemmatize:
+            return [token.lemma_ for token in self.processor(text)]
+        else:
+            return [token.text for token in self.processor(text)]
+
