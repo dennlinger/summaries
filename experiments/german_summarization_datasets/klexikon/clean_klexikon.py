9ca368db88ee83f7b0c11926097ac9337b744e6d
Dennis Aumiller <dennis.aumiller@gmx.de>
2022-08-29 17:20:59 +0200

    Running first analysis on Klexikon, finding 11 samples removed.

---
+++ experiments/german_summarization_datasets/klexikon/clean_klexikon.py
@@ -0,0 +1,27 @@
+"""
+Verify that the cleaner works as intended, and look for issues in Klexikon.
+"""
+
+from datasets import load_dataset
+from summaries.preprocessing import Cleaner
+from summaries.analysis import Analyzer
+
+
+def fuse_sentences_but_keep_sample(sample):
+    sample["wiki_text"] = "\n".join(sample["wiki_sentences"])
+    sample["klexikon_text"] = "\n".join(sample["klexikon_sentences"])
+
+    return sample
+
+
+if __name__ == '__main__':
+    klexikon = load_dataset("dennlinger/klexikon")
+
+    analyzer = Analyzer(lemmatize=True, lang="de")
+    cleaner = Cleaner(analyzer, min_length_summary=20, min_length_reference=50, length_metric="char")
+
+    train = [fuse_sentences_but_keep_sample(sample) for sample in klexikon["train"]]
+    validation = [fuse_sentences_but_keep_sample(sample) for sample in klexikon["validation"]]
+    test = [fuse_sentences_but_keep_sample(sample) for sample in klexikon["test"]]
+
+    clean_klexikon = cleaner.clean_dataset("klexikon_text", "wiki_text", train, validation, test)
