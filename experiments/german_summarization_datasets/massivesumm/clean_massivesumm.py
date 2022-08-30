4ac444da22be034f268fb81d8d06dd5a843949b3
Dennis Aumiller <dennis.aumiller@gmx.de>
2022-08-30 16:41:32 +0200

    Adding a more commented version of the experiments run on massivesumm, with a custom print details function for reference.

--- experiments/german_summarization_datasets/massivesumm/clean_massivesumm.py
+++ experiments/german_summarization_datasets/massivesumm/clean_massivesumm.py
@@ -1,11 +1,23 @@
 """
 Applying cleaner to Massivesumm.
 """
+from typing import Dict
 import json
 
 from summaries.analysis import Analyzer
 from summaries.preprocessing import Cleaner
-from summaries.preprocessing.Cleaner import example_print_details
+
+
+def custom_print_details(summary: str, reference: str, full_sample: Dict,
+                         filter_reason: str, analyzer: Analyzer, split: str) \
+        -> None:
+    """
+    Example of a print_details function implementation.
+    This will print the reference and summary if the sample has been filtered out for any reason.
+    """
+    if filter_reason is not None and len(summary) <= 20:
+        print(f"{full_sample}")
+
 
 if __name__ == '__main__':
     data_path = "/home/dennis/massivesumm/deu.all.jsonl"
@@ -22,9 +34,14 @@ if __name__ == '__main__':
     print(f"{len(train)} samples before filtering.")
 
     analyzer = Analyzer(lemmatize=True, lang="de")
-    cleaner = Cleaner(analyzer, min_length_summary=20, min_length_reference=50, length_metric="char",
-                      extractiveness=(0.10, 0.90))
-                      # extractiveness="fully")
+    # Analysis with minimal length requirements set
+    cleaner = Cleaner(analyzer, length_metric="char", min_length_summary=20, min_length_reference=50,
+                      extractiveness="fully")
+    # # Alternative analysis that does not impose length requirements
+    # cleaner = Cleaner(analyzer, extractiveness="fully")
 
-    clean_massivesumm = cleaner.clean_dataset("summary", "text", train, enable_tqdm=True,
-                                              print_details=example_print_details)
+    clean_massivesumm = cleaner.clean_dataset("summary", "text", train, enable_tqdm=True)
+
+    # # To investigate samples a bit more, you can additionally pass a function, as defined above:
+    # clean_massivesumm = cleaner.clean_dataset("summary", "text", train, enable_tqdm=True,
+    #                                           print_details=custom_print_details)
