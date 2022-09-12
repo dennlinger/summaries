a43cc08fc43edbe24fd37280545754fc536f6720
Dennis Aumiller <dennis.aumiller@gmx.de>
2022-09-12 14:23:55 +0200

    Adding cleaning results for EUR-Lex-Sum.

--- experiments/german_summarization_datasets/eurlexsum/clean_eurlexsum.py
+++ experiments/german_summarization_datasets/eurlexsum/clean_eurlexsum.py
@@ -1,3 +1,28 @@
 """
 Our dataset!
 """
+
+import json
+
+from summaries import Analyzer, Cleaner
+
+def extract_samples(split):
+    samples = []
+    for celex_id, sample in split.items():
+        samples.append(sample)
+
+    return samples
+
+
+if __name__ == '__main__':
+    with open("/home/dennis/german_eurlexsum/german_eurlexsum.json") as f:
+        dataset = json.load(f)
+    train = extract_samples(dataset["train"])
+    validation = extract_samples(dataset["validation"])
+    test = extract_samples(dataset["test"])
+
+    analyzer = Analyzer(lemmatize=True, lang="de")
+    cleaner = Cleaner(analyzer, deduplication_method="test_first", min_length_summary=20, min_length_reference=50,
+                      length_metric="char", extractiveness="fully")
+
+    clean_split = cleaner.clean_dataset("summary_text", "reference_text", train, validation, test, enable_tqdm=True)
