6bab5dd8ffb1fed53acdced256cf9b835b07c261
Dennis Aumiller <dennis.aumiller@gmx.de>
2022-09-16 14:39:01 +0200

    Adding other models for MLSUM eval.

--- experiments/german_summarization_datasets/baselines_generation/inference_mlsum.py
+++ experiments/german_summarization_datasets/baselines_generation/inference_mlsum.py
@@ -10,10 +10,14 @@ from utils import get_dataset, get_rouge_scores
 
 
 def get_summarizer_pipeline(model_name: str, batch_size: int = 16):
-    if "bert" in model_name:
-        model_max_length = 512
+    # Chosen cutoff by ourselves
+    if "ml6team" in model_name:
+        model_max_length = 768
+    # In model card
+    elif "T-Systems-onsite" in model_name:
+        model_max_length = 800
     else:
-        model_max_length = 1024
+        model_max_length = 512
     tokenizer = AutoTokenizer.from_pretrained(model_name, model_max_length=model_max_length)
     model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
     device = 1 if torch.cuda.is_available() else -1
@@ -23,8 +27,13 @@ def get_summarizer_pipeline(model_name: str, batch_size: int = 16):
 
 
 if __name__ == '__main__':
-    model_name = "mrm8488/bert2bert_shared-german-finetuned-summarization"
-    pipe = get_summarizer_pipeline(model_name, batch_size=16)
+    model_names = [
+        # "mrm8488/bert2bert_shared-german-finetuned-summarization",
+        "Shahm/t5-small-german",
+        "Einmalumdiewelt/T5-Base_GNAD",
+        "ml6team/mt5-small-german-finetune-mlsum",
+        "T-Systems-onsite/mt5-small-sum-de-en-v2",
+    ]
 
     # MLSUM
     name = "mlsum"
@@ -33,27 +42,30 @@ if __name__ == '__main__':
 
     eval_rouge_scores = True
 
-    for do_filter in [False, True]:
-        if do_filter:
-            filtered = "filtered"
-        else:
-            filtered = "unfiltered"
+    for model_name in model_names:
+        pipe = get_summarizer_pipeline(model_name, batch_size=16)
 
-        data = get_dataset(name, filtered=do_filter)
+        for do_filter in [False, True]:
+            if do_filter:
+                filtered = "filtered"
+            else:
+                filtered = "unfiltered"
 
-        for split in ["validation", "test"]:
-            print(f"Computing {filtered} {split} split...")
-            samples = data[split]
+            data = get_dataset(name, filtered=do_filter)
 
-            # Extract reference texts only.
-            reference_texts = [sample[reference_column] for sample in samples]
-            summary_texts = [sample[summary_column] for sample in samples]
+            for split in ["validation", "test"]:
+                print(f"Computing {filtered} {split} split...")
+                samples = data[split]
 
-            generated_summaries = [generation["summary_text"] for generation in
-                                   pipe(reference_texts, min_length=10, no_repeat_ngram_size=3, truncation=True)]
+                # Extract reference texts only.
+                reference_texts = [sample[reference_column] for sample in samples]
+                summary_texts = [sample[summary_column] for sample in samples]
 
-            with open(f"{name}_{split}_{filtered}_{model_name.replace('/', '__')}.json", "w") as f:
-                json.dump(generated_summaries, f, ensure_ascii=False, indent=2)
+                generated_summaries = [generation["summary_text"] for generation in
+                                       pipe(reference_texts, min_length=10, no_repeat_ngram_size=3, truncation=True)]
 
-            if eval_rouge_scores:
-                aggregator = get_rouge_scores(summary_texts, generated_summaries)
+                with open(f"{name}_{split}_{filtered}_{model_name.replace('/', '__')}.json", "w") as f:
+                    json.dump(generated_summaries, f, ensure_ascii=False, indent=2)
+
+                if eval_rouge_scores:
+                    aggregator = get_rouge_scores(summary_texts, generated_summaries)
