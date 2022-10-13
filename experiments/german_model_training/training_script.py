f19705c47824e8b20b406fdf2d3533c99785d609
Dennis Aumiller <dennis.aumiller@gmail.com>
2022-10-13 17:50:29 +0200

    Adding float32 run of mt5-small on our internal server.

--- experiments/german_model_training/training_script.py
+++ experiments/german_model_training/training_script.py
@@ -84,7 +84,7 @@ if __name__ == '__main__':
         max_length = 256
         summary_max_length = 128
     else:
-        model_name = "google/mt5-base"
+        model_name = "google/mt5-small"
         max_length = 768
         summary_max_length = 512
 
