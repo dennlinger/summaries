7b3f74a4604905d9f5173aba359c0574e88f09c3
Dennis Aumiller <dennis.aumiller@gmx.de>
2023-05-12 17:12:43 +0200

    Adjusting AspectSummarizer to work for newly created architecture.

--- summaries/AspectSummarizer.py
+++ summaries/AspectSummarizer.py
@@ -2,71 +2,75 @@
 Base class file, containing the holistic pipeline.
 """
 
-from typing import Union, List
+import warnings
+from typing import Union, Optional
+from itertools import count
 
 from spacy.language import Language
 
-from .extractors import Extractor, YakeExtractor
-from .retrievers import Retriever, FrequencyRetriever, DPRRetriever
-from .generators import Generator
+from .extractors import Extractor, LeadExtractor
+from .generators import Generator, OpenAIGenerator, ExtractiveGenerator, VALID_OPENAI_MODEL_NAMES
 from .index import Index
-from .utils import get_nlp_model
+from .document import Document, Segment
+from .utils import get_nlp_model, interpret_lang_code
 
 
 class AspectSummarizer:
-
-    extractor: Extractor
-    # FIXME: Merge Retriever into extractor class?
-    retriever: Retriever
-    generator: Generator
-    index: Index
+    # Meta-information regarding the pre-processing of documents.
+    segment_level: str
+    lang: str
     processor: Language
+    # Generation-side part of the model part of the model.
+    generator: Generator
+    # Storage solution for the intermediate document representation.
+    index: Index
+    segments = list[Segment]
 
-    def __init__(self, extractor: Union[str, Extractor] = "yake", retriever: Union[str, Retriever] = "frequency"):
+    def __init__(self,
+                 segment_level: str = "sentence",
+                 lang: str = "en",
+                 generator: Union[str, Generator] = "gpt-3.5-turbo"):
+        """
+        # TODO Explain init function.
+        :param segment_level: Specifies at what granularity an input text is split. Currently supports
+            - "document" (document-level distinction, primarily intended for short texts, e.g., social media posts)
+            - "paragraph" (paragraph-level split, segmenting at double newlines "\n\n")
+            - "sentence" (default value; splits at the sentence-level with the help of spaCy)
+        :param lang: Language identifier or code for the language in which texts are. Default is English.
+        :param generator: What
+        """
+        self.segment_level = segment_level
+        self.lang = interpret_lang_code(lang)
         # FIXME: Proper loading of model with arguments
-        self.processor = get_nlp_model("sm", disable=tuple("ner"), lang="de")
+        self.processor = get_nlp_model("sm", disable=tuple("ner"), lang=self.lang)
 
         # TODO: Enable passing of config files with attributes
-        self._assign_extractor(extractor, retriever)
         self._assign_generator(generator)
 
-    def _assign_extractor(self, extractor: Union[str, Extractor], retriever: Union[str, Retriever]) -> None:
-        """
-        Determine whether an existing Extractor object was passed, or otherwise create appropriate Extractor object.
-        :param extractor: Either existing extractor object or string identifier for a model.
-        :return: valid Extractor object
-        """
-        # FIXME: Can cause problems with custom classes overwriting base!
-        if isinstance(extractor, Extractor):
-            self.extractor = extractor
-        elif extractor == "yake":
-            self.extractor = YakeExtractor(num_topics=5, max_ngram_size=3, lang="de")
-        else:
-            raise NotImplementedError("Extractor component not yet supported!")
-
-        # FIXME: Can cause problems with custom classes overwriting base!
-        if isinstance(retriever, Retriever):
-            self.retriever = retriever
-        elif retriever.lower() == "frequency":
-            self.retriever = FrequencyRetriever(self.processor)
-        elif retriever.lower() == "dpr":
-            self.retriever = DPRRetriever("deepset/gbert-base-germandpr-question_encoder",
-                                          "deepset/gbert-base-germandpr-ctx_encoder")
-        else:
-            raise NotImplementedError("Retriever component not yet supported!")
+        # Initialize empty representation
+        self.segments = []
 
     def _assign_generator(self, generator: Union[str, Generator]) -> None:
 
         # FIXME: Can cause problems with custom classes overwriting base!
         if isinstance(generator, Generator):
             self.generator = generator
+        elif generator in VALID_OPENAI_MODEL_NAMES:
+            self.generator = OpenAIGenerator(model_name=generator)
+        elif generator == "extractive":
+            self.generator = ExtractiveGenerator()
         else:
             raise NotImplementedError("Other Generator models currently not supported!")
 
-    def summarize(self, source_text: Union[str, list[str]], ex_ante_aspects: dict, ex_post_aspects: dict):
+    def summarize(self, source_texts: Optional[Union[str, list[str]]] = None,
+                  ex_ante_aspects: Optional[dict] = None,
+                  ex_post_aspects: Optional[dict] = None,
+                  segment_limit_intermediate_representation: Optional[int] = None):
         """
-
-        :param source_text: Single input text, or otherwise collection (list) of several input texts.
+        Generates an aspect-focused summary over a collection of documents, based on user-specified query-parameters.
+        :param source_texts: Single input text, or otherwise collection (list) of several input texts.
+            TODO: Improve design for repeated queries to the same input document collection
+            Leave empty (None) if you want to generate summaries over a previously specified document collection
         :param ex_ante_aspects: Dictionary containing the desired ex-ante (extractive) aspects and the parameters.
             # TODO: Add list of all accepted parameters
             Example:
@@ -77,58 +81,90 @@ class AspectSummarizer:
                 }
         :param ex_post_aspects: Dictionary containing the desired ex-post (generative) aspects and their parameters.
             # TODO: Add list of all accepted parameters
+        :param segment_limit_intermediate_representation: Limits the number of segments passed to the ex-post stage,
+            based on the "most relevant" segments from the ex-ante stage. This utilizes a function based on simple
+            linear combination, but # TODO: can be adjusted in the init function if necessary.
         :return: An aspect-focused summary generated with the respective specified aspects.
         """
+        # Skip processing of documents if querying an existing collection
+        if source_texts is not None:
+            # Ensure consistent representation between single/multiple texts
+            if isinstance(source_texts, str):
+                source_texts = [source_texts]
 
-    def topic_summarize(self, source_text: Union[str, list[str]], max_length: int = -1):
-        """
-        TODO: Refactor to reflect changes that are no longer in line with the proposed model!
-        Extractively summarizes documents based on a simple topic-aggregation strategy.
-        :param source_text: Document text(s) that should be summarized.
-        :param max_length: Maximum length (in number of sentences) for the target summary.
-        :return: Aspect-based summary of the text
-        """
-        topics = self.extractor.extract_keywords(source_text)
-        self.build_index(source_text)
-        summary_sentence_ids = []
-        # Set sensible per-topic limit if max_length is specified. Otherwise, .retrieve() will reset to default.
-        if max_length > 0:
-            limit = max_length // len(topics) + 1
+            # Process input documents into segments
+            self._segment_documents(source_texts)
+
+        if ex_ante_aspects is None and ex_post_aspects is None:
+            warnings.warn("No aspects specified; will generate generic summary.")
+
+        if source_texts is None and len(self.segments) == 0:
+            raise ValueError("No internal representation available!"
+                             "You might have accidentally overwritten the previously stored documents,"
+                             "or forgot to pass segments along with the specified aspects.")
+
+        # Do extractive filtering part.
+        for aspect, param_variants in ex_ante_aspects.items():
+            for param in param_variants:
+                extractor = self._assign_extractor(aspect, param)
+
+                extractor.rank(self.segments)
+
+        # Generate summary
+        # TODO: Adjust inputs to the following function call
+        if segment_limit_intermediate_representation is None:
+            aspect_summary = self.generator.generate(self.segments, ex_ante_aspects, ex_post_aspects)
+        # Filter to a smaller intermediate subset if desired
         else:
-            limit = 3
-        for topic in topics:
-            summary_sentence_ids.extend(self.retriever.retrieve(topic, self.index, limit=limit))
-        # Heuristic to order the chosen sentence in order of occurrence (i.e., our assigned ID)
-        summary_sentence_ids = sorted(set(summary_sentence_ids))
-        summary_sentences = [self.index.doc_lookup[idx] for idx in summary_sentence_ids]
-        if max_length > 0:
-            return "\n".join(summary_sentences)[:max_length]
-        return "\n".join(summary_sentences)
+            passed_segments = self._filter_intermediate_segments(segment_limit_intermediate_representation)
+            aspect_summary = self.generator.generate(passed_segments, ex_ante_aspects, ex_post_aspects)
 
-    def build_index(self, sources: Union[str, List[str]]):
+        return aspect_summary
+
+    def clarify_summary(self, clarification_query: str) -> str:
         """
-        Depending on single or multiple documents, will build an inverted index based on "sentence documents",
-        i.e., each sentence representing a different document.
-        :param sources: Single or multiple documents from which to summarize
-        :return: Sentence-based Index
+        Based on an intermediate summary representation, clarify parts of a summary with the original documents.
+        :param clarification_query: String of a clarification aspect
+        :return: Answer clarifying missing information.
         """
-        if isinstance(sources, str):
-            sources = [sources]
-            sources = self.split_into_sentence_documents(sources)
-        if isinstance(sources, list):
-            # Assume that it is already split into sentences
-            pass
+
+        raise NotImplementedError("Currently not implemented!")
+
+    def _segment_documents(self, source_documents: list[str]) -> None:
+        """
+        Will turn the provided documents into a more structured format,
+        including meta-annotation etc.
+        :param source_documents:
+        :return: No return value, assigns segments internally to class attribute.
+        """
+        id_counter = count(0)
+
+        for document in source_documents:
+            doc = Document(self.processor(document),
+                           document_id=next(id_counter),
+                           segment_level=self.segment_level)
+            for segment in doc:
+                self.segments.append(segment)
+
+    # TODO: Determine best output function?
+    def _filter_intermediate_segments(self, segment_limit: int):
+        """
+        Implements a naive filtering method, selecting the most relevant segments up to a certain number.
+        Overall relevance is computed by the sum over individual aspect dimensions.
+        :return: Subset of segments, with the highest relevance.
+        """
+        return sorted(self.segments, key=lambda seg: sum(seg.relevance_vector), reverse=True)[:segment_limit]
+
+    @staticmethod
+    def _assign_extractor(aspect, param) -> Extractor:
+        if aspect == "lead":
+            if isinstance(param, int):
+                extractor = LeadExtractor(fixed_cutoff=param)
+            elif callable(param):
+                extractor = LeadExtractor(decayed_relevance_func=param)
+            else:
+                raise ValueError("Unrecognized parameter for lead-based ex-ante filter.")
         else:
-            # TODO: Currently our indexing strategy of assigning a u_id doesn't work with multiple document.
-            #  If we want to utilize ordering on the final ids, then this implicitly forces a document order,
-            #  which might be unwanted.
-            raise NotImplementedError("Multi-document summarization currently not supported!")
+            raise NotImplementedError("Not implemented yet!")
 
-        self.index = Index(sources, self.retriever.processor)
-
-    def split_into_sentence_documents(self, source_documents: List[str]) -> List[str]:
-        sentences = []
-        for doc_text in source_documents:
-            sentences.extend([sentence.text for sentence in self.processor(doc_text).sents])
-        return sentences
-
+        return extractor
